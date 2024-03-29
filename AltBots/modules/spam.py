13c1a87844d8d05d1748f695570e42644a87091f
SAIFDEAD <145872388+SAIFDEAD@users.noreply.github.com>
2024-03-29 23:48:36 +0530

    ⚡ 𝐒αιғβσтƨ ⚡

--- AltBots/modules/spam.py
+++ AltBots/modules/spam.py
@@ -80,7 +80,7 @@ async def spam(event: events):
 async def pspam(event):
     if event.sender_id in SUDO_USERS:
         if event.chat_id in GROUP:
-            await event.reply("» 𝚂𝙾𝚁𝚁𝚈, 𝚃𝙷𝙸𝚂 𝙸𝚂 𝙳𝙴𝙰𝙳 𝙿𝚁𝙾𝚃𝙴𝙲𝚃𝙴𝙳 𝙶𝚁𝙾𝚄𝙿 ⚡✨.")
+            await event.reply("» 𝚂𝙾𝚁𝚁𝚈, 𝚃𝙷𝙸𝚂 𝙸𝚂 𝙳𝙴𝙰𝙳 𝙿𝚁𝙾𝚃𝙴𝙲𝚃𝙴𝙳 𝙶𝚁𝙾𝚄𝙿 .")
         else:
             try:
                 counter = int(event.text.split(" ", 2)[1])
@@ -108,7 +108,7 @@ async def pspam(event):
 async def hang(e):
     if e.sender_id in SUDO_USERS:
         if e.chat_id in GROUP:
-            await e.reply("» 𝚂𝙾𝚁𝚁𝚈, 𝚃𝙷𝙸𝚂 𝙸𝚂 𝙳𝙴𝙰𝙳 𝙿𝚁𝙾𝚃𝙴𝙲𝚃𝙴𝙳 𝙶𝚁𝙾𝚄𝙿 ⚡✨.")
+            await e.reply("» 𝚂𝙾𝚁𝚁𝚈, 𝚃𝙷𝙸𝚂 𝙸𝚂 𝙳𝙴𝙰𝙳 𝙿𝚁𝙾𝚃𝙴𝙲𝚃𝙴𝙳 𝙶𝚁𝙾𝚄𝙿 .")
         else:
             try:
                 counter = int(e.text.split(" ", 2)[1])
