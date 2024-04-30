a17bb9c8aef5a296fa4bf801321b004825a6fe5c
OXYEGN <166620351+BWANDARLOK@users.noreply.github.com>
2024-04-30 23:36:36 +0530

    Update wish.py

--- AltBots/modules/wish.py
+++ AltBots/modules/wish.py
@@ -1,9 +1,8 @@
+import asyncio
 from random import choice
-
 from telethon import events
-
 from config import X1, X2, X3, X4, X5, X6, X7, X8, X9, X10, SUDO_USERS, OWNER_ID, CMD_HNDLR as hl
-from AltBots.data import GM, GN, GA, ALTRON
+from AltBots.data import GM, GA, GN
 
 @X1.on(events.NewMessage(incoming=True, pattern=r"\%sgm(?: |$)(.*)" % hl))
 @X2.on(events.NewMessage(incoming=True, pattern=r"\%sgm(?: |$)(.*)" % hl))
@@ -16,7 +15,7 @@ from AltBots.data import GM, GN, GA, ALTRON
 @X9.on(events.NewMessage(incoming=True, pattern=r"\%sgm(?: |$)(.*)" % hl))
 @X10.on(events.NewMessage(incoming=True, pattern=r"\%sgm(?: |$)(.*)" % hl))
 async def gm(e):
-    if e.sender_id in SUDO_USERS:
+     if e.sender_id in SUDO_USERS:
         xraid = e.text.split(" ", 2)
 
         if len(xraid) == 3:
@@ -29,19 +28,16 @@ async def gm(e):
             uid = entity.id
 
         try:
-            if uid in ALTRON:
-                await e.reply("𝙽𝙾, 𝚃𝙷𝙸𝚂 𝙶𝚄𝚈 𝙸𝚂 𝙳𝙴𝚅𝙻𝙾𝙿𝙴𝚁 𝙾𝙵 𝙳𝙴𝙰𝙳 𝙱𝙾𝚃 .")
-            else:
-                first_name = entity.first_name
-                counter = int(xraid[1])
-                username = f"[{first_name}](tg://user?id={uid})"
-                for _ in range(counter):
-                    reply = choice()
-                    caption = f"{username} {reply}"
-                    await e.client.send_message(e.chat_id, caption)
-                    await asyncio.sleep(0.1)
+            first_name = entity.first_name
+            counter = int(xraid[1])
+            username = f"[{first_name}](tg://user?id={uid})"
+            for _ in range(counter):
+                reply = choice(GM)
+                caption = f"{username} {reply}"
+                await e.client.send_message(e.chat_id, caption)
+                await asyncio.sleep(0.1)
         except (IndexError, ValueError, NameError):
-            await e.reply(f"𝗠𝗼𝗱𝘂𝗹𝗲 𝗡𝗮𝗺𝗲: 𝐆𝐨𝐨𝐝𝐦𝐨𝐫𝐧𝐢𝐧𝐠\n  » {hl}gm <ᴄᴏᴜɴᴛ> <ᴜꜱᴇʀɴᴀᴍᴇ ᴏꜰ ᴜꜱᴇʀ>\n  » {hl}gm <ᴄᴏᴜɴᴛ> <ʀᴇᴘʟʏ ᴛᴏ ᴀ ᴜꜱᴇʀ>")
+            await e.reply(f"{hl}ɢᴍ <ᴄᴏᴜɴᴛ> <ᴜꜱᴇʀɴᴀᴍᴇ ᴏꜰ ᴜꜱᴇʀ> <ʀᴇᴘʟʏ ᴛᴏ ᴀ ᴜꜱᴇʀ>")
         except Exception as e:
             print(e)
 
@@ -56,7 +52,7 @@ async def gm(e):
 @X9.on(events.NewMessage(incoming=True, pattern=r"\%sga(?: |$)(.*)" % hl))
 @X10.on(events.NewMessage(incoming=True, pattern=r"\%sga(?: |$)(.*)" % hl))
 async def ga(e):
-    if e.sender_id in SUDO_USERS:
+     if e.sender_id in SUDO_USERS:
         xraid = e.text.split(" ", 2)
 
         if len(xraid) == 3:
@@ -69,19 +65,16 @@ async def ga(e):
             uid = entity.id
 
         try:
-            if uid in ALTRON:
-                await e.reply("𝙽𝙾, 𝚃𝙷𝙸𝚂 𝙶𝚄𝚈 𝙸𝚂 𝙳𝙴𝚅𝙻𝙾𝙿𝙴𝚁 𝙾𝙵 𝙳𝙴𝙰𝙳 𝙱𝙾𝚃 .")
-            else:
-                first_name = entity.first_name
-                counter = int(xraid[1])
-                username = f"[{first_name}](tg://user?id={uid})"
-                for _ in range(counter):
-                    reply = choice(GA)
-                    caption = f"{username} {reply}"
-                    await e.client.send_message(e.chat_id, caption)
-                    await asyncio.sleep(0.1)
+            first_name = entity.first_name
+            counter = int(xraid[1])
+            username = f"[{first_name}](tg://user?id={uid})"
+            for _ in range(counter):
+                reply = choice(GA)
+                caption = f"{username} {reply}"
+                await e.client.send_message(e.chat_id, caption)
+                await asyncio.sleep(0.1)
         except (IndexError, ValueError, NameError):
-            await e.reply(f"𝗠𝗼𝗱𝘂𝗹𝗲 𝗡𝗮𝗺𝗲: 𝐆𝐨𝐨𝐝𝐄𝐯𝐧𝐢𝐧𝐠\n  » {hl}ga <ᴄᴏᴜɴᴛ> <ᴜꜱᴇʀɴᴀᴍᴇ ᴏꜰ ᴜꜱᴇʀ>\n  » {hl}ga <ᴄᴏᴜɴᴛ> <ʀᴇᴘʟʏ ᴛᴏ ᴀ ᴜꜱᴇʀ>")
+            await e.reply(f"{hl}ɢᴀ <ᴄᴏᴜɴᴛ> <ᴜꜱᴇʀɴᴀᴍᴇ ᴏꜰ ᴜꜱᴇʀ> <ʀᴇᴘʟʏ ᴛᴏ ᴀ ᴜꜱᴇʀ>")
         except Exception as e:
             print(e)
 
@@ -96,7 +89,7 @@ async def ga(e):
 @X9.on(events.NewMessage(incoming=True, pattern=r"\%sgn(?: |$)(.*)" % hl))
 @X10.on(events.NewMessage(incoming=True, pattern=r"\%sgn(?: |$)(.*)" % hl))
 async def gn(e):
-    if e.sender_id in SUDO_USERS:
+     if e.sender_id in SUDO_USERS:
         xraid = e.text.split(" ", 2)
 
         if len(xraid) == 3:
@@ -109,18 +102,15 @@ async def gn(e):
             uid = entity.id
 
         try:
-            if uid in ALTRON:
-                await e.reply("𝙽𝙾, 𝚃𝙷𝙸𝚂 𝙶𝚄𝚈 𝙸𝚂 𝙳𝙴𝚅𝙻𝙾𝙿𝙴𝚁 𝙾𝙵 𝙳𝙴𝙰𝙳 𝙱𝙾𝚃 .")
-            else:
-                first_name = entity.first_name
-                counter = int(xraid[1])
-                username = f"[{first_name}](tg://user?id={uid})"
-                for _ in range(counter):
-                    reply = choice(GN)
-                    caption = f"{username} {reply}"
-                    await e.client.send_message(e.chat_id, caption)
-                    await asyncio.sleep(0.1)
+            first_name = entity.first_name
+            counter = int(xraid[1])
+            username = f"[{first_name}](tg://user?id={uid})"
+            for _ in range(counter):
+                reply = choice(GN)
+                caption = f"{username} {reply}"
+                await e.client.send_message(e.chat_id, caption)
+                await asyncio.sleep(0.1)
         except (IndexError, ValueError, NameError):
-            await e.reply(f"𝗠𝗼𝗱𝘂𝗹𝗲 𝗡𝗮𝗺𝗲: 𝐆𝐨𝐨𝐝𝐍𝐲𝐭\n  » {hl}gn <ᴄᴏᴜɴᴛ> <ᴜꜱᴇʀɴᴀᴍᴇ ᴏꜰ ᴜꜱᴇʀ>\n  » {hl}gn <ᴄᴏᴜɴᴛ> <ʀᴇᴘʟʏ ᴛᴏ ᴀ ᴜꜱᴇʀ>")
+            await e.reply(f"{hl}ɢɴ <ᴄᴏᴜɴᴛ> <ᴜꜱᴇʀɴᴀᴍᴇ ᴏꜰ ᴜꜱᴇʀ> <ʀᴇᴘʟʏ ᴛᴏ ᴀ ᴜꜱᴇʀ>")
         except Exception as e:
             print(e)
